5e9ccf3f0b2fa4f0447852b2dccef740ff7a2760
Oleksandr Kovalenko <133910408+SashaTheOk@users.noreply.github.com>
2024-01-08 12:33:46 +0100

    1

---
+++ 01_tread_like_funct.py
@@ -0,0 +1,10 @@
+from threading import Thread
+
+def worker(arg):
+    print(arg)
+
+if __name__ == '__main__':
+    
+    for i in range(5):
+        th = Thread(target=worker, args=(f'count th- {i}',))
+        th.start()
